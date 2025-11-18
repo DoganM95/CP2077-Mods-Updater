82a446b9f178b8d3788e36b55ae2315a4732cf17
DoganM95 <doganmermertas@hotmail.de>
2025-11-18 14:03:53 +0100

    optimize index.js

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 require("dotenv").config({ path: __dirname + "/.env" });
 require("dotenv").config({ path: __dirname + "/private.env" });
 const fs = require("node:fs");
-const path = require("node:path");
-const os = require("node:os");
-const { exec: _exec } = require("node:child_process");
-const axios = require("axios");
 const util = require("./util");
 
 const GAME_DIR = process.env.GAME_DIR || "/game";
@@ -23,17 +19,11 @@ if (!fs.existsSync(TMP_DIR)) fs.mkdirSync(TMP_DIR, { recursive: true });
         util.log(`ERROR: GAME_DIR ${GAME_DIR} does not exist. Mount the game folder at /game in Docker.`);
         process.exit(2);
     }
-
-    if (process.env.RUN_ONCE == "true") {
-        await util.runOnce();
-        process.exit(0);
-    }
-
-    util.log(`Starting CP2077 Node.js updater. Poll interval = ${POLL_INTERVAL}s`);
-
+    util.log(`Starting CP2077 Node.js updater. ${process.env.RUN_ONCE == "true" ? "" : "Poll interval = " + POLL_INTERVAL + "s"}`);
     while (true) {
         await util.runOnce();
         util.log(`Cycle complete. Sleeping ${POLL_INTERVAL}s`);
         await new Promise((r) => setTimeout(r, POLL_INTERVAL));
+        if (process.env.RUN_ONCE == "true") process.exit(0);
     }
 })();
