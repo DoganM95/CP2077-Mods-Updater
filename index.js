2898f013e3a235610b6b63b6e65b1de9924e82ee
DoganM95 <doganmermertas@hotmail.de>
2025-11-18 12:13:36 +0100

    use private env file in code

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config({ path: __dirname + "/.env" });
+require("dotenv").config({ path: __dirname + "/private.env" });
 const fs = require("node:fs");
 const path = require("node:path");
 const os = require("node:os");
